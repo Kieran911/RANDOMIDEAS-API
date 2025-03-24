da1451e460b9c0b46aa0070697ee873ac5c411c9
Kieran911 <arinzekieran@gmail.com>
2025-03-24 13:02:33 +0100

    refactor: update Idea model and routes to use mongoose for CRUD operations

--- models/Idea.js
+++ models/Idea.js
@@ -4,16 +4,16 @@ const IdeaSchema = new mongoose.Schema({
   text: {
     type: String,
     required: [true, 'Please add a text field'],
-    tag: {
-      type: String,
-    },
-    username: {
-      type: String,
-    },
-    date: {
-      type: Date,
-      default: Date.now,
-    },
+  },
+  tag: {
+    type: String,
+  },
+  username: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
   },
 });
 
